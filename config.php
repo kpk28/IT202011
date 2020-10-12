7f99fab5b82e68701372bb2777ab2d44f127fd06
kpk28 <47124146+kpk28@users.noreply.github.com>
2020-10-11 22:15:08 -0400

    lib folder upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+//this file you'll have to create directly on the VM or use FTP to move it
+//this should not and will not be on github, -points if it's seen on github.
+
+//for everyone else fill in these variable strings with the connection
+//details from your respective servers and delete the Heroku block above
+$dbhost = "sql1.njit.edu";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
+$dbuser = "kpk28";//your ucid since we follow this pattern
+$dbpass = "Supersonic!2#";//database password generated or chosen per respective steps
+$dbdatabase = "kpk28";//your ucid since we follow this pattern
+?>
